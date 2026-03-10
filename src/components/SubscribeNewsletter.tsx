9093ae795e801b21785500e92c27084858953ce0
Borna Almasi <borna@borna-almasi.com>
2026-03-09 22:39:48 -0400

    ui: make consent larger

--- src/components/SubscribeNewsletter.tsx
+++ src/components/SubscribeNewsletter.tsx
@@ -55,9 +55,8 @@ export default function SubscribeNewsletter() {
         </div>
 
         {/* Consent Note */}
-        <p className="mt-6 text-white/40 text-[10px] leading-relaxed">
+        <p className="mt-6 text-white/40 text-[14px] leading-relaxed">
           By subscribing, you agree to our <a href="/terms" className="underline hover:text-white/60">Terms of Service</a> and <a href="/privacy" className="underline hover:text-white/60">Privacy Policy</a>. 
-          You can unsubscribe at any time.
         </p>
 
       </div>
